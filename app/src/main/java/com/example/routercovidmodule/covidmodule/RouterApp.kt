d2442305f855d050c7ced29025ef403abc66f87c
fabian <fabian.guerrero@carsales.com>
2022-10-24 16:55:07 -0300

    fragment creation + rputer module method

--- app/src/main/java/com/example/routercovidmodule/covidmodule/RouterApp.kt
+++ app/src/main/java/com/example/routercovidmodule/covidmodule/RouterApp.kt
@@ -1,4 +1,10 @@
 package com.example.routercovidmodule.covidmodule
 
+import androidx.fragment.app.Fragment
+import com.example.covidmodule.main_module.view.MainViewFragment
+
 class RouterApp {
+    fun getFragment(): Fragment {
+        return MainViewFragment.newInstance()
+    }
 }
